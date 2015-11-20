12c9f19edbdd6e9479ca97f55c8373cf16d7fde8
grw <georgerw@gmail.com>
2015-11-20 13:57:59 +0000

    sbnet

--- services/tinc/tinc.nix
+++ services/tinc/tinc.nix
@@ -2,17 +2,21 @@
 
 {
   services.tinc.networks."darknet" = {
-    package = pkgs.tinc_pre;
     hosts = {
-      tsar = ''
+      tsar_su = ''
         Address = tsar.su
         Ed25519PublicKey = /hjseAhOR1tAiittJSgwO4lFG8yO42Pq9rXHfeF47ZN
+        Subnet 10.0.0.1/32
       '';
       nixhost = ''
         Address = 86.3.184.2
         Ed25519PublicKey = GC1+3QeMQZwvuRKFsHUd8Mw6vKbCl3uG66M6xGqhXwB
+        Subnet 10.0.0.2/32
       '';
     };
+    extraConfig = ''
+      ConnectTo tsar_su
+    '';
   };
   networking.firewall.allowedUDPPorts = [ 655 ];
 }
