eb3caf5d2489d1e3495eb3ed7faa9a96015b2dca
George Whewell <georgerw@gmail.com>
2015-11-22 22:04:36 +0000

    adds gogs service

--- services/gogs.nix
+++ services/gogs.nix
@@ -1,5 +1,28 @@
 { config, lib, pkgs, ... }:
 
 {
- networking.firewall.allowedTCPPorts = [ 10080 2222];
+  networking.firewall.allowedTCPPorts = [ 10080 2222];
+
+  systemd.services.gogs = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    requires = [ "docker.service" ];
+    serviceConfig = {
+      TimeoutStartSec = 0;
+      Restart = "always";
+      ExecStartPre = [
+        ''-${pkgs.docker}/bin/docker pull gogs/gogs''
+        ''-${pkgs.docker}/bin/docker stop gogs''
+        ''-${pkgs.docker}/bin/docker rm gogs''
+      ];
+      ExecStart = ''${pkgs.docker}/bin/docker run \
+        --name gogs \
+        -p 3000:3000 \
+        -p 2222:22 \
+        -v /mnt/gogs:/data \
+        gogs/gogs'';
+      ExecStop = ''${pkgs.docker}/bin/docker stop gogs'';
+    };
+  };
+  
 }
