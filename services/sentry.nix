4f67aca9306c7f864797416c4385688cbef08451
grw <georgerw@gmail.com>
2015-11-26 15:53:44 +0000

    sentry nginx

--- services/sentry.nix
+++ services/sentry.nix
@@ -3,20 +3,42 @@
 {
   services.nginx.httpConfig = ''
     server {
-        listen 443;
+       listen 80;
+       server_name sentry.tsar.su;
+       rewrite ^(.*) https://$host$1 permanent;
+    }
+
+    server {
+        listen 443 ssl;
         server_name sentry.tsar.su;
+
         ssl_certificate /etc/letsencrypt/live/git.tsar.su/fullchain.pem;
         ssl_certificate_key /etc/letsencrypt/keys/0011_key-letsencrypt.pem;
+        ssl_session_cache shared:SSL:128m;
+        ssl_session_timeout 10m;
+        
+        proxy_set_header   Host                 $http_host;
+        proxy_set_header   X-Forwarded-Proto    $scheme;
+        proxy_set_header   X-Forwarded-For      $remote_addr;
+        proxy_redirect     off;
+
+        # keepalive + raven.js is a disaster
+        keepalive_timeout 0;
+
+        # use very aggressive timeouts
+        proxy_read_timeout 5s;
+        proxy_send_timeout 5s;
+        send_timeout 5s;
+        resolver_timeout 5s;
+        client_body_timeout 5s;
+
+        # buffer larger messages
+        client_max_body_size 5m;
+        client_body_buffer_size 100k;
 
         location / {
-            proxy_pass http://127.0.0.1:9000/;
-
-            proxy_set_header        Accept-Encoding   "";
-            proxy_set_header        Host            $host;
-            proxy_set_header        X-Real-IP       $remote_addr;
-            proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header        X-Forwarded-Proto $scheme;
-            proxy_redirect          off;
+          proxy_pass        http://localhost:9000;
+          add_header Strict-Transport-Security "max-age=31536000";
         }
     }
   '';
