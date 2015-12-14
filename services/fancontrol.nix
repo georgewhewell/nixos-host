ba342d8e269dff9ceef576ce3c564dea1752b9ff
grw <georgerw@gmail.com>
2015-12-14 01:20:23 +0000

    be quiet

--- services/fancontrol.nix
+++ services/fancontrol.nix
@@ -3,6 +3,7 @@
 {
   jobs.fancontrol = {
     description = "fancontrol daemon";
+wantedBy = [ "multi-user.target" ];
     exec = "${pkgs.lm_sensors}/sbin/fancontrol /etc/nixos/fancontrol";
   };
 }
