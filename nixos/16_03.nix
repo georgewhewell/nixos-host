21e68f1cb76e988320f11cdcc84ea9fd772899f1
grw <georgerw@gmail.com>
2016-02-05 23:18:18 +0000

    nix config

--- nixos/16_03.nix
+++ nixos/16_03.nix
@@ -7,6 +7,21 @@
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "16.03";
 
-  nix.binaryCachePublicKeys = [
-"hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs=" ];
+  nix = {
+    buildCores = 0;
+    daemonIONiceLevel = 7;
+    daemonNiceLevel = 10;
+    useChroot = true;
+    extraOptions = ''
+      auto-optimise-store = true
+    '';
+    trustedBinaryCaches = [
+      https://cache.nixos.org
+      https://hydra.nixos.org
+    ];
+    binaryCachePublicKeys = [
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+    ];
+  };
+  
 }
