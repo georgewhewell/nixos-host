eaaa20984566cd48a05c11eef30ef6e904393410
grw <georgerw@gmail.com>
2016-02-06 02:41:17 +0000

    break

--- nixos/16_03.nix
+++ nixos/16_03.nix
@@ -2,7 +2,7 @@
 
 {
   # Save this file!
-  system.copySystemConfiguration = true;
+#  system.copySystemConfiguration = true;
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "16.03";
