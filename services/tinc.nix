a19e1b309b5495eadf071e5cfdf37d2e8196e222
grw <georgerw@gmail.com>
2016-02-08 18:26:29 +0000

    use autodiscover for tinc

--- services/tinc.nix
+++ services/tinc.nix
@@ -6,23 +6,18 @@
     hosts = {
       tsar_su = ''
 Address = tsar.su
-Subnet 10.0.0.1
 Ed25519PublicKey = Uv0u+p5xpxjqVDcFjFaS2wkmcjMkLiDo5fzIOlqs0XH
       '';
       nixhost = ''
-Subnet 10.0.0.2
 Ed25519PublicKey = pFg5yd5fvc2ZkfyKhJj550qXNNMxYVVkF+Ig5y9Z8dC
       '';
       Georges-Mac-Pro = ''
-Subnet 10.0.0.3
 Ed25519PublicKey = 8mnpkMJutf+tSnZFYGBB32YMnIwsdXzSQpoM1xB1wKN
 '';
       h9fp4whfi_local = ''
-Subnet 10.0.0.4
 Ed25519PublicKey = BPDO5MqPCAfVroAeGNO0QJ/EJigO0HXhl6eeJqwH4hC
 '';
      iPhone = ''
-Subnet 10.0.0.5
 Ed25519PublicKey = HgbMtGmOQB65ObsD0a2Q1S+Gm+UKzjL7ty6Dv4jlEqK
 '';
 	};
