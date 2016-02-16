4c8034e05de334ce071eab397d85ca75f5befbac
grw <georgerw@gmail.com>
2016-02-16 23:57:39 +0000

    merge?

--- nixhost.nix
+++ nixhost.nix
@@ -106,4 +106,53 @@
 
     ./users.nix
   ];
+
+services.collectd = {  
+     enable = true;  
+     extraConfig = ''  
+        # Interval at which to query values. Can be overwritten on per plugin  
+        # with the 'Interval' option.  
+        # WARNING: You should set this once and then never touch it again. If  
+        # you do, you will have to delete all your RRD files.  
+        Interval 10  
+        # Load plugins  
+        LoadPlugin apcups  
+        LoadPlugin contextswitch  
+        LoadPlugin cpu  
+        LoadPlugin df  
+        LoadPlugin disk  
+        LoadPlugin ethstat  
+        LoadPlugin interface  
+        LoadPlugin irq  
+        LoadPlugin libvirt  
+        LoadPlugin load  
+        LoadPlugin memory  
+        LoadPlugin network  
+        LoadPlugin nfs  
+        LoadPlugin processes  
+        LoadPlugin rrdtool  
+        LoadPlugin sensors  
+        LoadPlugin tcpconns  
+        LoadPlugin uptime  
+        <Plugin "virt">  
+          Connection "qemu:///system"  
+        </Plugin>  
+        # Ignore some paths/filesystems that cause "Permission denied" spamming  
+        # in the log and/or are uninteresting or duplicates.  
+        <Plugin "df">  
+          MountPoint "/var/lib/docker/devicemapper"  
+          MountPoint "/nix/store"  # it's just a bind mount, already covered  
+          FSType "fuse.gvfsd-fuse"  
+          FSType "cgroup"  
+          FSType "tmpfs"  
+          FSType "devtmpfs"  
+          IgnoreSelected true  
+        </Plugin>  
+        # Output/write plugin (need at least one, if metrics are to be persisted)  
+        <Plugin "network">  
+          Server "tsar.su" "25826"  
+        </Plugin>  
+      '';  
+};  
+
 }
--- server.nix
+++ server.nix
@@ -67,9 +67,10 @@ boot.kernel.sysctl = {
   '';
   services.munin-node.enable = true;
   services.redis.enable = true;
-
-  services.influxdb.enable = true;
-  services.influxdb.extraConfig.collectd.enabled = true;
+  security.pam.loginLimits = [
+    { domain = "redis"; item = "nofile"; type = "soft"; value = 65536; }
+    { domain = "redis"; item = "nofile"; type = "hard"; value = 65536; }
+  ];
 
   services.fail2ban.enable = true;
   # services.fail2ban.jails.ssh-iptables = "enabled = true";
@@ -91,54 +92,6 @@ boot.kernel.sysctl = {
     { device = "zpool/docker";
       fsType = "zfs";
     };
-
-  services.collectd = {
-     enable = true;
-     extraConfig = ''
-        # Interval at which to query values. Can be overwritten on per plugin
-        # with the 'Interval' option.
-        # WARNING: You should set this once and then never touch it again. If
-        # you do, you will have to delete all your RRD files.
-        Interval 10
-        # Load plugins
-        LoadPlugin apcups
-        LoadPlugin contextswitch
-        LoadPlugin cpu
-        LoadPlugin df
-        LoadPlugin disk
-        LoadPlugin ethstat
-        LoadPlugin interface
-        LoadPlugin irq
-        LoadPlugin libvirt
-        LoadPlugin load
-        LoadPlugin memory
-        LoadPlugin network
-        LoadPlugin nfs
-        LoadPlugin processes
-        LoadPlugin rrdtool
-        LoadPlugin sensors
-        LoadPlugin tcpconns
-        LoadPlugin uptime
-        <Plugin "virt">
-          Connection "qemu:///system"
-        </Plugin>
-        # Ignore some paths/filesystems that cause "Permission denied" spamming
-        # in the log and/or are uninteresting or duplicates.
-        <Plugin "df">
-          MountPoint "/var/lib/docker/zfs"
-          MountPoint "/nix/store"  # it's just a bind mount, already covered
-          FSType "fuse.gvfsd-fuse"
-          FSType "cgroup"
-          FSType "tmpfs"
-          FSType "devtmpfs"
-          IgnoreSelected true
-        </Plugin>
-        # Output/write plugin (need at least one, if metrics are to be persisted)
-        <Plugin "network">
-          Server "tsar.su" "25826"
-        </Plugin>
-      '';
-  };
 
   imports = [
     <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
