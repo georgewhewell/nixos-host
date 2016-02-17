01ccaaf55e6c2ea1eca4b0eac828fef9cd318b95
grw <georgerw@gmail.com>
2016-02-17 00:54:31 +0000

    collectd plugins

--- services/collectd.nix
+++ services/collectd.nix
@@ -15,22 +15,24 @@
         LoadPlugin cpu
         LoadPlugin df
         LoadPlugin disk
-        LoadPlugin ethstat
         LoadPlugin interface
         LoadPlugin irq
-        LoadPlugin libvirt
         LoadPlugin load
         LoadPlugin memory
         LoadPlugin network
         LoadPlugin processes
-        LoadPlugin rrdtool
         LoadPlugin sensors
         LoadPlugin tcpconns
         LoadPlugin uptime
+        LoadPlugin redis
+        LoadPlugin nginx
+        LoadPlugin postgresql
 
         # Ignore some paths/filesystems that cause "Permission denied" spamming
         # in the log and/or are uninteresting or duplicates.
         <Plugin "df">
+          MountPoint "/var/lib/docker/*"
+          MountPoint "/var/lib/docker"
           MountPoint "/var/lib/docker/zfs"
           MountPoint "/var/lib/docker/containers"
           MountPoint "/nix/store"  # it's just a bind mount, already covered
