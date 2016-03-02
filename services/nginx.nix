670a050dbea3531965bd7255d8413524dec28007
George Whewell <georgerw@gmail.com>
2016-03-02 12:41:36 +0000

    adds acme for root domain

--- services/nginx.nix
+++ services/nginx.nix
@@ -3,22 +3,24 @@
 {
   networking.firewall.allowedTCPPorts = [ 80 443 2222 ];
 
+  security.acme.certs."tsar.su" = {
+     	email = "georgerw@gmail.com";
+      webroot = "/var/www/challenges/";
+  };
+
   services.nginx.enable = true;
   services.nginx.httpConfig = ''
     server {
-        listen 80 default;
-        listen 443 ssl;
+       listen 80 default;
+       server_name tsar.su;
 
-        server_name tsar.su www.tsar.su;
+       location /.well-known/acme-challenge/ {
+           alias /var/www/challenges/.well-known/acme-challenge/;
+       }
 
-        ssl_certificate /etc/letsencrypt/live/git.tsar.su/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/keys/0011_key-letsencrypt.pem;
-
-        root /var/www/munin;
-
-        location / {
-          add_header Strict-Transport-Security "max-age=31536000";
-        }
+       location / {
+         rewrite ^(.*) https://$host$1 permanent;
+       }
     }
     server {
       listen 82 default;
@@ -26,6 +28,27 @@
       location /basic_status {
           stub_status;
       }
+    }
+    server {
+        listen 443 ssl default;
+        server_name tsar.su;
+
+        ssl_certificate /var/lib/acme/tsar.su/fullchain.pem;
+        ssl_certificate_key /var/lib/acme/tsar.su/key.pem;
+        ssl_session_cache shared:SSL:128m;
+        ssl_session_timeout 10m;
+
+        location / {
+            proxy_pass http://127.0.0.1:8005;
+
+            proxy_set_header        Host            $host;
+            proxy_set_header        X-Real-IP       $remote_addr;
+            proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header        X-Forwarded-Proto $scheme;
+            proxy_redirect     off;
+
+            add_header Strict-Transport-Security "max-age=31536000";
+        }
     }
   '';
 }
