165101d15182f5a80109844b577e6458cc9ad192
George Whewell <georgerw@gmail.com>
2016-06-26 19:40:18 +0100

    cert for bucket

--- services/ceph.nix
+++ services/ceph.nix
@@ -7,10 +7,15 @@
       webroot = "/var/www/challenges/";
   };
 
+  security.acme.certs."musicowl.ceph.tsar.su" = {
+      email = "georgerw@gmail.com";
+      webroot = "/var/www/challenges/";
+  };
+
   services.nginx.httpConfig = ''
     server {
        listen 80;
-       server_name ceph.tsar.su;
+       server_name .ceph.tsar.su;
 
        location /.well-known/acme-challenge/ {
            alias /var/www/challenges/.well-known/acme-challenge/;
@@ -19,6 +24,28 @@
        location / {
          rewrite ^(.*) https://$host$1 permanent;
        }
+    }
+
+    server {
+        listen 127.0.0.1:443 ssl;
+        server_name musicowl.ceph.tsar.su;
+
+        ssl_certificate /var/lib/acme/musicowl.ceph.tsar.su/fullchain.pem;
+        ssl_certificate_key /var/lib/acme/musicowl.ceph.tsar.su/key.pem;
+        ssl_session_cache shared:SSL:128m;
+        ssl_session_timeout 10m;
+
+        proxy_set_header   Host                 $http_host;
+        proxy_set_header   X-Forwarded-Proto    $scheme;
+        proxy_set_header   X-Forwarded-For      $remote_addr;
+        proxy_redirect     off;
+
+        client_max_body_size 128m;
+
+        location / {
+          proxy_pass        http://localhost:8050;
+          add_header Strict-Transport-Security "max-age=31536000";
+        }
     }
 
     server {
