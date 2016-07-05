43d489406dcf866a76220353149635df99ce9b53
georgewhewell <george.whewell@artsalliancemedia.com>
2016-07-05 12:20:02 +0000

    i3 fotns

--- i3.nix
+++ i3.nix
@@ -2,15 +2,45 @@
 { pkgs, ... }:
 
 {
-  environment.x11Packages = with pkgs; [
+nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
     dmenu     # for app launcher
     feh       # for background image
+    i3
     i3lock    # screen lock
     i3status  # sys info
     scrot     # for screenshot
+    rxvt_unicode
   ];
 
-  hardware.opengl.driSupport32Bit = true;
+  fonts.fontconfig = {
+    defaultFonts = {
+      monospace = [ "Inconsolata" "Source Code Pro" "DejaVu Sans Mono" ];
+      sansSerif = [ "Ubuntu" "Liberation Sans" "DejaVu Sans" ];
+    };
+    ultimate = {
+      rendering = {
+        INFINALITY_FT_FILTER_PARAMS = "08 24 36 24 08";
+        INFINALITY_FT_FRINGE_FILTER_STRENGTH = "25";
+        INFINALITY_FT_USE_VARIOUS_TWEAKS = "true";
+        INFINALITY_FT_WINDOWS_STYLE_SHARPENING_STRENGTH = "25";
+        INFINALITY_FT_STEM_ALIGNMENT_STRENGTH = "15";
+        INFINALITY_FT_STEM_FITTING_STRENGTH = "15";
+      };
+    };
+  };
+  fonts.fonts = with pkgs; [
+    corefonts
+    source-han-sans-japanese
+    source-han-sans-korean
+    source-han-sans-simplified-chinese
+    source-code-pro
+    dejavu_fonts
+    ubuntu_font_family
+    inconsolata
+    libertine
+    unifont
+  ];
 
   services.xserver = {
     enable = true;
@@ -22,7 +52,6 @@
     };
 
     displayManager = {
-      slim.enable = true;
       sessionCommands = ''
         i3status &
       '';
