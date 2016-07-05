7ecc0bd983455f875669a6b2ae167aefa228feb0
grw <grw@tsar.su>
2016-07-05 13:20:09 +0100

    ceph

--- services/ceph.nix
+++ services/ceph.nix
@@ -5,80 +5,44 @@
   security.acme.certs."ceph.tsar.su" = {
       email = "georgerw@gmail.com";
       webroot = "/var/www/challenges/";
-  };
-
-  security.acme.certs."musicowl.ceph.tsar.su" = {
-      email = "georgerw@gmail.com";
-      webroot = "/var/www/challenges/";
+      extraDomains = {
+        "musicowl.ceph.tsar.su" = null;
+      };
   };
 
   services.nginx.httpConfig = ''
     server {
        listen 80;
-       server_name .ceph.tsar.su;
+       server_name musicowl.ceph.tsar.su ceph.tsar.su;
 
        location /.well-known/acme-challenge/ {
            alias /var/www/challenges/.well-known/acme-challenge/;
        }
 
        location / {
+         add_header Strict-Transport-Security "max-age=31536000";
          rewrite ^(.*) https://$host$1 permanent;
        }
     }
 
     server {
         listen 127.0.0.1:443 ssl;
-        server_name musicowl.ceph.tsar.su;
-
-        ssl_certificate /var/lib/acme/musicowl.ceph.tsar.su/fullchain.pem;
-        ssl_certificate_key /var/lib/acme/musicowl.ceph.tsar.su/key.pem;
-        ssl_session_cache shared:SSL:128m;
-        ssl_session_timeout 10m;
-
-        proxy_set_header   Host                 $http_host;
-        proxy_set_header   X-Forwarded-Proto    $scheme;
-        proxy_set_header   X-Forwarded-For      $remote_addr;
-        proxy_redirect     off;
-
-        client_max_body_size 10g;
-
-        location / {
-            if ($request_method = PUT) {
-                rewrite ^ /PUT$request_uri;
-            }
-            proxy_pass        http://localhost:8050;
-            add_header Strict-Transport-Security "max-age=31536000";
-        }
-
-        location /PUT/ {
-            internal;
-            proxy_pass        http://localhost:8050;
-            add_header Strict-Transport-Security "max-age=31536000";
-        }
-    }
-
-    server {
-        listen 127.0.0.1:443 ssl;
-        server_name ceph.tsar.su;
+        server_name ceph.tsar.su musicowl.ceph.tsar.su;
 
         ssl_certificate /var/lib/acme/ceph.tsar.su/fullchain.pem;
         ssl_certificate_key /var/lib/acme/ceph.tsar.su/key.pem;
         ssl_session_cache shared:SSL:128m;
         ssl_session_timeout 10m;
-
-        proxy_set_header   Host                 $http_host;
-        proxy_set_header   X-Forwarded-Proto    $scheme;
-        proxy_set_header   X-Forwarded-For      $remote_addr;
-        proxy_redirect     off;
-
-        client_max_body_size 128m;
+        
+        client_max_body_size 1G;
+        dav_methods PUT;
 
         location / {
-          proxy_pass        http://localhost:8050;
-          add_header Strict-Transport-Security "max-age=31536000";
+            proxy_set_header Host $host;
+            proxy_pass http://localhost:8050;
         }
-    }
-  '';
+   }
+'';
 
   systemd.services.ceph = {
     wantedBy = [ "multi-user.target" ];
@@ -93,8 +57,8 @@
         -v /zpool/ceph/demostack/etc:/etc/ceph \
         -v /zpool/ceph/demostack/ceph:/var/lib/ceph \
         -e MON_IP=176.9.138.4 \
-        -e RGW_CIVETWEB_PORT=8050 \
         -e CEPH_NETWORK=172.17.0.1/32 \
+        -e RGW_CIVETWEB_PORT=8050 \
         ceph/demo
         '';
     };
