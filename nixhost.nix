3b9e4f43d3aa8dddfc97d14a7e3a9f3cca4fe615
George Whewell <georgerw@gmail.com>
2016-08-28 20:49:19 +0100

    modules

--- nixhost.nix
+++ nixhost.nix
@@ -1,48 +1,111 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  nixpkgs.config.allowUnfree = true;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.extraModulePackages = [ ];
+  networking.hostName = "nixhost";
+  networking.hostId = "dd499341";
 
   time.timeZone = "Europe/London";
 
-  networking.hostName = "nixhost";
-  networking.hostId = "deadbeef";
-  #networking.bridges.br0 = {
-  #  interfaces = ["eno1" "eno2" "eno3" "eno4"];
-  #};
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    defaultLocale = "en_GB.UTF-8";
+  };
 
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    htop
+    tmux
+    iptables
+    lm_sensors
+    sdparm
+    smartmontools
+    libvirt
+    zfs
+  ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  services.avahi.enable = true;
+  services.avahi.publish.enable = true;
+  services.avahi.publish.addresses = true;
+  services.avahi.nssmdns = true;
+
+  services.openssh.enable = true;
+  services.thermald.enable = true;
+  services.munin-node.enable = true;
+
+  hardware.pulseaudio.enable = true;
+
+  services.redshift = {
+    enable = true;
+    latitude = "51.5";
+    longitude = "-0.1";
+  };
+
+  services.fail2ban.enable = true;
+
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "nvme" "dm_mod" "zfs" ];
+
+  fileSystems."/boot" =
+    { device = "/dev/nvme0n1p1";
+      fsType = "vfat";
+    };
   fileSystems."/" =
-    { device = "fpool/root/nixos";
+    { device = "zpool/nixos";
+      fsType = "zfs";
+    };
+  fileSystems."/mnt/storage" =
+    { device = "zpool/storage";
+      fsType = "zfs";
+    };
+  fileSystems."/var/lib/docker" =
+    { device = "zpool/docker";
+      fsType = "zfs";
+    };
+  fileSystems."/mnt/Home" =
+      { device = "zpool/Home";
+        fsType = "zfs";
+      };
+  fileSystems."/mnt/Media" =
+    { device = "zpool/Media";
       fsType = "zfs";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1A02-B98B";
-      fsType = "vfat";
+  fileSystems."/config" =
+    { device = "/dev/disk/by-id/dm-name-sm951-config";
+      fsType = "btrfs";
     };
 
-  swapDevices = [ ];
+  boot.kernelParams = [
+    "i915.preliminary_hw_support=1"
+    "usbhid.mousepoll=1"
+  ];
 
-  nix.maxJobs = lib.mkDefault 24;
+  boot.loader.gummiboot.enable = true;
+  boot.loader.gummiboot.timeout = 1;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
+  imports = [
+    ./i3.nix
+    ./nixos/16_03.nix
+    ./modules/custom-packages.nix
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = false;
+#   ./network/wlan.nix
+#   ./services/tinc.nix
 
-  # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "17.03";
+#   ./services/collectd.nix
+#   ./services/fancontrol.nix
+#   ./services/samba.nix
+#   ./services/docker.nix
+#   ./services/sonarr.nix
+#   ./services/couchpotato.nix
+#   ./services/transmission.nix
+#   ./services/upnpc.nix
+#   ./services/ethminer.nix
 
-  imports =
-    [
-      ./users.nix
-    ];
+    ./users.nix
+  ];
+
 }
