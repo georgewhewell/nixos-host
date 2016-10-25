85304ab25d99187724bb16974c713db956bec267
grw <grw@tsar.su>
2016-10-25 16:38:52 +0100

    fix docker

--- services/docker.nix
+++ services/docker.nix
@@ -3,6 +3,8 @@
 {
   virtualisation.docker = {
     enable = true;
+    storageDriver = "zfs";
+    socketActivation = false;
     extraOptions = "-H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock";
   };
 }
