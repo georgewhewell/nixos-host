29a657ec6937ec3b34cea1325efbd33528acd7dd
George Whewell <grw@FUCKUP.local>
2016-12-03 14:30:18 +0000

    add more firewall holes for plex

--- services/plex.nix
+++ services/plex.nix
@@ -7,7 +7,8 @@
       fsType = "zfs";
     };
 
-  networking.firewall.allowedTCPPorts = [ 32400 ];
+  networking.firewall.allowedTCPPorts = [ 32400 32469 3005 ];
+  networking.firewall.allowedUDPPorts = [ 5353 32410 32412 32413 32414 ]
 
   systemd.services.plex = {
     wantedBy = [ "multi-user.target" ];
@@ -21,7 +22,7 @@
         --net="host" \
         -e VERSION=latest \
         -e PUID=${toString config.ids.uids.transmission} \
-        -e PGID=${toString config.gids.gids.transmission} \
+        -e PGID=${toString config.ids.gids.transmission} \
         -e TZ=UTC \
         -v /var/lib/plex:/config \
         -v /mnt/Media/TV:/data/tvshows \
