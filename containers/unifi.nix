a1f4803019e09172a60207be86ab9e236662c8b7
georgewhewell <georgerw@gmail.com>
2016-12-03 21:03:35 +0000

    persist unifi data

--- containers/unifi.nix
+++ containers/unifi.nix
@@ -1,26 +1,41 @@
 { config, lib, pkgs, boot, networking, containers, ... }:
 
 {
+
+  fileSystems."/var/lib/unifi" =
+    { device = "fpool/root/config/unifi";
+      fsType = "zfs";
+    };
+
   containers.unifi = {
 
     autoStart = true;
     privateNetwork = true;
     hostBridge = "br0";
 
+    bindMounts = {
+      "/var/lib/unifi/data" = {
+        hostPath = "/var/lib/unifi/data";
+        isReadOnly = false;
+      };
+    };
+
     config = {
       boot.isContainer = true;
 
       networking.hostName = "unifi";
-      networking.firewall.allowedTCPPorts = [ 8443 ];
-
       networking.interfaces.eth0.useDHCP = true;
-
-      services.avahi.nssmdns = true;
-      services.avahi.enable = true;
-      services.avahi.publish.enable = true;
+      networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [ 443 8443 ];
+        extraCommands = ''
+          ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8443
+        '';
+      };
 
       nixpkgs.config.allowUnfree = true;
       services.unifi.enable = true;
+
     };
   };
 }
