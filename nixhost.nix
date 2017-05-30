94bf6d9e0f148f63ff52be076dbf908321056ef2
georgewhewell <georgerw@gmail.com>
2017-05-30 18:39:26 +0100

    nixhost updates

--- nixhost.nix
+++ nixhost.nix
@@ -12,11 +12,13 @@
 
   time.timeZone = "Europe/London";
 
-  networking.hostName = "nixhost";
-  networking.hostId = "deadbeef";
-
-  networking.bridges.br0 = {
-    interfaces = ["eno1" "eno2" "eno3" "eno4"];
+  networking = {
+    hostName = "nixhost";
+    hostId = "deadbeef";
+    useDHCP = true;
+    bridges.br0 = {
+      interfaces = ["eno1" "eno2" "eno3" "eno4"];
+    };
   };
 
   fileSystems."/" =
@@ -37,9 +39,11 @@
   swapDevices = [ ];
 
   nix.maxJobs = lib.mkDefault 24;
+  nixpkgs.config.allowUnfree = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.openssh.forwardX11 = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = false;
@@ -53,14 +57,24 @@
   services.avahi.nssmdns = true;
   services.avahi.interfaces = ["br0"];
 
-  # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "17.03";
+  networking.firewall = {
+    checkReversePath = false;
+    trustedInterfaces = [ "cbr0" ];
+    allowedTCPPorts = [ 6789 ];
+  };
 
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = true;
     "net.ipv6.conf.all.forwarding" = true;
     "net.ipv4.conf.all.proxy_arp" = true;
   };
+ 
+  virtualisation.libvirtd.enable = true;
+
+  hardware.bluetooth.enable = true;
+  hardware.enableAllFirmware = true;
+
+  #hardware.firmware = [ pkgs.BCM20702A1 ];
 
   imports =
     [
@@ -70,7 +84,6 @@
       ./containers/couchpotato.nix
       ./containers/sonarr.nix
       ./containers/plex.nix
-      ./services/k8s.nix
       ./services/nfs.nix
       ./services/netatalk.nix
       ./services/grafana.nix
