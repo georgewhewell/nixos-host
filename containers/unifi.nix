d704a091af817558fdd5ccd85de3ad6968dca743
georgewhewell <georgerw@gmail.com>
2017-05-30 18:29:53 +0100

    restart unifi controller 5s after boot

--- containers/unifi.nix
+++ containers/unifi.nix
@@ -30,11 +30,22 @@
         allowedTCPPorts = [ 443 8443 ];
         extraCommands = ''
           ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8443
+          ${pkgs.iptables}/bin/iptables -t nat -A OUTPUT -o lo -p tcp --dport 443 -j REDIRECT --to-port 8443
         '';
       };
 
       nixpkgs.config.allowUnfree = true;
       services.unifi.enable = true;
+
+      systemd.services.unfuck_unifi = {
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          TimeoutStartSec = "5s";
+          ExecStart = ''
+            ${pkgs.systemd}/bin/systemctl restart unifi
+          '';
+        };
+      };
 
     };
   };
