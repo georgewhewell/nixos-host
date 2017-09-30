89ef9e515f973e8719e7626ffd0dc0d3c7e6b9b3
grw <georgerw@gmail.com>
2017-09-30 18:10:24 +0100

    router config

--- router.nix
+++ router.nix
@@ -3,33 +3,35 @@
 {
   imports =
     [
-      # Include the results of the hardware scan.
-      ./users.nix
-      ./nixos/17_03.nix
+      ./profiles/common.nix
+      ./profiles/home.nix
+      ./profiles/uefi-boot.nix
     ];
 
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = true;
-    "net.ipv6.conf.all.forwarding" = true;
-    "net.ipv4.conf.all.proxy_arp" = true;
-  };
+  sound.enable = false;
 
-  hardware.enableAllFirmware = true;
-  hardware.cpu.intel.updateMicrocode = true;
-
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
-
-  services.vnstat = {
-    enable = true;
-  };
-
+  services.haveged.enable = true;
+  services.vnstat.enable = true;
   services.fail2ban.enable = true;
   services.fail2ban.jails.ssh-iptables = "enabled = true";
-  
+  services.thermald.enable = true;
+
+  nix.maxJobs = lib.mkDefault 4;
+  powerManagement.cpuFreqGovernor = "performance";
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/dd3984c7-ebec-4e35-91dc-2e176ed8e788";
+      fsType = "btrfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/DBE8-FF96";
+      fsType = "vfat";
+    };
+
   networking = {
     hostName = "router"; # Define your hostname.
+    hostId = "deadbeef";
     useNetworkd = true;
 
     nameservers = [ "127.0.0.1" ];
@@ -45,6 +47,7 @@
       forwardPorts = [
         { sourcePort = 80; destination = "192.168.23.175:80"; }
         { sourcePort = 443; destination = "192.168.23.175:443"; }
+        { sourcePort = 51413; destination = "192.168.23.175:51413"; }
       ];
     };
 
@@ -81,45 +84,11 @@
       cname=hydra.satanic.link,nixhost.4a
       cname=grafana.satanic.link,nixhost.4a
       cname=git.satanic.link,nixhost.4a
+      cname=elk.satanic.link,nixhost.4a
       log-dhcp
     '';
 
   };
-
-  time.timeZone = "Europe/London";
-
-  services.openssh.enable = true;
-  services.openssh.permitRootLogin = "yes";
-
-  services.haveged.enable = true;
-  services.thermald.enable = true;
-  services.avahi.enable = true;
-
-  sound.enable = false;
-
-  system.stateVersion = "18.03"; # Did you read the comment?
-
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_acpi" "r8169" "mii" "tpm" "tpm_tis" ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/dd3984c7-ebec-4e35-91dc-2e176ed8e788";
-      fsType = "btrfs";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DBE8-FF96";
-      fsType = "vfat";
-    };
-
-  swapDevices = [ ];
-
-  nix.maxJobs = lib.mkDefault 4;
-  powerManagement.cpuFreqGovernor = "performance";
 
   systemd.services.prometheus-node-exporter = {
     wantedBy = [ "multi-user.target" ];
