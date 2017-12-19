164881faf0393d15ec32a2c55e8bef85d8d8a882
georgewhewell <georgerw@gmail.com>
2017-12-19 22:06:55 +0000

    set nfs opts

--- profiles/nas-mounts.nix
+++ profiles/nas-mounts.nix
@@ -1,17 +1,20 @@
 { config, pkgs, ... }:
 
-{
+
+let
+  options = [ "nofail" "rsize=32768" "wsize=32768" ];
+in {
 
   fileSystems."/mnt/Home" =
     { device = "nixhost.4a:/home";
       fsType = "nfs";
-      options = [ "nofail" ];
+      inherit options;
     };
 
   fileSystems."/mnt/nixhostconfig" =
     { device = "nixhost.4a:/nixos-config";
       fsType = "nfs";
-      options = [ "nofail" ];
+      inherit options;
     };
 
 }
