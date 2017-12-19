799ba0624087c69872b0d4129e1b692867e690f8
georgewhewell <georgerw@gmail.com>
2017-12-19 18:37:46 +0000

    yoga

--- yoga.nix
+++ yoga.nix
@@ -42,6 +42,12 @@
     usbmuxd
     gnupg
     powertop
+    usbutils
+    msr-tools
+    /*validity90*/
+    auto-rotate
+    steam
+    rfkill
   ];
 
   imports =
@@ -54,29 +60,5 @@
       ./profiles/thinkpad.nix
       ./profiles/g_ether.nix
     ];
-
-  networking.bridges."br0" = {
-    interfaces = [];
-  };
-
-  networking.interfaces."br0" = {
-    /*networking.interfaces."usb0" = {*/
-    ipAddress = "10.10.10.1";
-    prefixLength = 24;
-  };
-
-  services.nfs.server = {
-    enable = true;
-    exports = ''
-      /export         *(rw,fsid=0,no_subtree_check)
-      /export/store   *(rw,nohide,all_squash,anonuid=1000,anongid=1000,insecure,no_subtree_check)
-    '';
-  };
-
-  networking.firewall.allowedTCPPorts = [
-    111  # nfs?
-    2049
-    20048 # nfs
-  ];
 
 }
