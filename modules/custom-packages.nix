5b2dab2b38807c27a3ce8ab27cc73b55fe9ae40d
georgewhewell <georgerw@gmail.com>
2017-12-19 18:29:50 +0000

    overrides

--- modules/custom-packages.nix
+++ modules/custom-packages.nix
@@ -1,27 +1,79 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.config = let
-    webscreensaver = pkgs.stdenv.mkDerivation {
-      name = "webscreensaver";
-      version = "0.1";
-      src = pkgs.fetchFromGithub {
-        owner = "lmartinking";
-        repo = "webscreensaver";
-        rev = "05a736c00fb66d902269d492d1a07febb7c4ed95";
-        sha256 = "0wq8lrial1khc0kv34g2n7wbl9bf9m3vfk29d51g6r0hg3vzp49l";
-      };
-      installPhase = ''
-        mkdir -p $out/bin
-        cp webscreensaver $out/bin/webscreensaver
-      '';
+  nixpkgs.overlays = [
+    (self: super: {
+      auto-rotate = pkgs.stdenv.mkDerivation {
+        name = "auto-rotate";
+        version = "auto-rotate";
 
-    };
-    in {
-    packageOverrides = pkgs: {
-      inherit (pkgs.callPackages ../packages/default.nix { })
-        si2168_02 couchpotato kubernetes_15 thin-provisioning-tools cni BCM20702A1
-        prometheus-snmp-exporter prometheus-ipmi-exporter jackett radarr headphones webscreensaver;
-    };
-  };
+        src = super.fetchFromGitHub {
+          owner = "mrquincle";
+          repo = "yoga-900-auto-rotate";
+          rev = "master";
+          sha256 = "0ibg4mxkdhf19pn7h4z9xjx6gh2i9r67869yz60bwzzfzybhyqj2";
+        };
+        buildInputs = with pkgs; [
+          iio-sensor-proxy
+          pkgconfig
+          glib
+          x11
+          xorg.libXrandr
+        ];
+        buildPhase = ''
+          make
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp auto-rotate $out/bin/
+        '';
+      };
+
+      validity90 = pkgs.stdenv.mkDerivation {
+        name = "validity90";
+        version = "unstable";
+        src = super.fetchFromGitHub {
+          owner = "nmikhailov";
+          repo = "Validity90";
+          rev = "00ac6ab7f54b012a8a0627fb389bd62ebf14c4fb";
+          sha256 = "04hdfp80ckwjljk5np358rx3vcfhf2fqvs9b7savsazrgm2p955l";
+        };
+        sourceRoot = "source/prototype/";
+        buildInputs = with pkgs; [
+          pkgconfig
+          glib.dev
+          gnutls
+          libusb
+          libgcrypt
+          libpng
+          nss
+          openssl.dev
+        ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp prototype $out/bin/validity90
+        '';
+      };
+
+      /*
+      libfprint = super.libfprint.overrideAttrs (old: {
+        src = super.fetchFromGitHub {
+          owner = "nmikhailov";
+          repo = "Validity90";
+          rev = "00ac6ab7f54b012a8a0627fb389bd62ebf14c4fb";
+          sha256 = "04hdfp80ckwjljk5np358rx3vcfhf2fqvs9b7savsazrgm2p955l";
+        };
+        sourceRoot = "source/libfprint/";
+        buildInputs = with pkgs; [
+          autoreconfHook
+          pkgconfig
+          libusb
+          nss
+          glib
+          pixman
+        ];
+      });
+      */
+    })
+  ];
 }
