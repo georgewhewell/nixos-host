646a8dd19d39809c19a3e8907401450cec99c75f
georgewhewell <georgerw@gmail.com>
2018-01-01 22:57:54 +0000

    transmission

--- services/transmission.nix
+++ services/transmission.nix
@@ -1,15 +1,18 @@
 { config, lib, pkgs, ... }:
 
-{
-  fileSystems."/var/lib/transmission/incomplete" =
-    { device = "fpool/root/downloads";
+let
+  incompleteDir = "/mnt/downloads";
+in {
+  fileSystems.${incompleteDir} =
+    { device = "bpool/root/downloads";
       fsType = "zfs";
+      options = [ "nofail" ];
     };
 
   services.transmission.enable = true;
   services.transmission.settings = {
     download-dir = "/mnt/Media/downloads";
-    incomplete-dir = "/var/lib/transmission/incomplete";
+    incomplete-dir = incompleteDir;
     incomplete-dir-enabled = true;
     rpc-whitelist = "127.0.0.1,192.168.23.*,192.168.24.*";
     umask = 0;
