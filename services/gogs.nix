33da759f342deb59e00d5dd27c27457f24d948ba
georgewhewell <georgerw@gmail.com>
2018-01-01 22:37:59 +0000

    git settings

--- services/gogs.nix
+++ services/gogs.nix
@@ -9,7 +9,19 @@
 
   services.gogs = {
     enable = true;
-    useWizard = true;
+    database = {
+      type = "postgres";
+      host = "127.0.0.1";
+      port = 5432;
+      passwordFile = "/etc/nix/gogs-pw";
+    };
+    domain = "git.satanic.link";
+    rootUrl = "https://git.satanic.link";
+    httpPort = 3001;
+    cookieSecure = true;
+    extraConfig = ''
+      SSH_PORT = 2222
+    '';
   };
 
   services.nginx.virtualHosts."git.satanic.link" = {
