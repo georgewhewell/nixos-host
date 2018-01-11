ef9579a74d9f4d422c63260c5ed946be6220e557
georgewhewell <georgerw@gmail.com>
2018-01-11 09:33:50 +0000

    manage i3 config

--- services/usbmuxd.nix
+++ services/usbmuxd.nix
@@ -11,4 +11,10 @@
       '';
     };
   };
+
+  services.udev.extraRules = ''
+    ENV{DEVTYPE}=="usb_device", ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="129[0-9abcef]", RUN+="${pkgs.ipad_charge}/bin/ipad_charge"
+    ENV{DEVTYPE}=="usb_device", ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="12a[0-9ab]", RUN+="${pkgs.ipad_charge}/bin/ipad_charge"
+    ENV{DEVTYPE}=="usb_device", ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0fca", ATTR{idProduct}=="8014", RUN+="${pkgs.ipad_charge}/bin/ipad_charge"
+  '';
 }
