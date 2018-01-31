e52e00c1b76764ec8473f383cb79b730bc2deef7
georgewhewell <george.whewell@aig.com>
2018-01-31 11:10:51 +0000

    vm profile

--- workvm.nix
+++ workvm.nix
@@ -12,24 +12,25 @@
       fsType = "zfs";
     };
 
-  networking.hostName = "nixos";
-  networking.hostId = "deadbeef";
-
-  networking.firewall = {
-    enable = true;
-    allowPing = false;
+  networking = {
+    hostName = "nixos";
+    hostId = "deadbeef";
+    useNetworkd = true;
+    useDHCP = true;
+    enableIPv6 = false;
+    firewall = {
+      enable = true;
+      allowPing = false;
+    };
   };
 
   time.timeZone = "Europe/London";
 
-  boot.initrd.supportedFilesystems = [ "zfs" ];
-  # List packages installed in system profile. To search by name, run:
   environment.systemPackages = with pkgs; [
     libreoffice
     firefox
   ];
 
-  nix.maxJobs = lib.mkDefault 2;
   virtualisation.virtualbox.guest.enable = true;
 
   imports = [
@@ -38,5 +39,6 @@
     ./profiles/gpg-yubikey.nix
     ./profiles/uefi-boot.nix
     ./profiles/xserver.nix
+    ./profiles/g_ether.nix
   ];
 }
