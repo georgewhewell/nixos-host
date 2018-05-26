4c7088e96d90ff0e6f049ee1dcb3355c9c84085c
georgewhewell <georgerw@gmail.com>
2018-05-26 18:32:12 +0000

    customize vim

--- profiles/common.nix
+++ profiles/common.nix
@@ -71,7 +71,6 @@
   environment.systemPackages = with pkgs; [
     acpi
     wget
-    vim
     rsync
     nox
     unzip
@@ -94,6 +93,23 @@
     pciutils
     wirelesstools
     rxvt_unicode
+    
+    (vim_configurable.customize {
+      # Specifies the vim binary name.
+      # E.g. set this to "my-vim" and you need to type "my-vim" to open this vim
+      # This allows to have multiple vim packages installed (e.g. with a different set of plugins)
+      name = "vim";
+      vimrcConfig.customRC = ''
+        # Here you can specify what usually goes into `~/.vimrc` 
+        syntax enable
+      '';
+      vimrcConfig.vam.pluginDictionaries = [ 
+        { names = [
+          "commentary"
+          "vim-elixir"
+          "youcompleteme"
+        ]; } ];
+    })
   ];
 
 }
