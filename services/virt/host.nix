7f5269a31fc48466880072f69b76d78b9fec3c4f
georgewhewell <georgerw@gmail.com>
2019-02-12 14:18:23 +0000

    keys, vbox

--- services/virt/host.nix
+++ services/virt/host.nix
@@ -4,11 +4,6 @@
   # Turn on virt
   virtualisation.libvirtd.enable = true;
 
-  /* Constantly complains about missing extension pack
-  virtualisation.virtualbox.host.enable = true;
-  nixpkgs.config.virtualbox.enableExtensionPack = true;
-  */
-
   environment.systemPackages = with pkgs; [
     virtmanager
     spice-gtk # fix usb redirect
