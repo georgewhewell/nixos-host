ada13db391a8b59b937a4b984929d5925dd1e4f1
georgewhewell <georgerw@gmail.com>
2019-02-12 11:42:32 +0000

    clean ngnix

--- services/nginx.nix
+++ services/nginx.nix
@@ -7,41 +7,36 @@
     enable = true;
     statusPage = true;
     recommendedTlsSettings = true;
+    recommendedGzipSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     virtualHosts = {
-      "hydra.satanic.link" = {
+/*      "hydra.satanic.link" = {
          forceSSL = true;
          enableACME = true;
          locations."/" = {
-           proxyPass = "http://hydra.4a:3000";
+           extraConfig = ''
+            # Defer resolving upstream to allow start when upstream down
+            resolver 192.168.23.1 valid=30s;
+            set $upstream hydra.4a;
+            proxy_pass http://$upstream:3000;
+
+            proxy_set_header Host $host;
+            proxy_set_header X-Forwarded-For $remote_addr;
+            proxy_set_header X-Forwarded-Proto https;
+           '';
          };
       };
-      "bstream.satanic.link" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "https://stream.binance.com:9443";
-          extraConfig = ''
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-          '';
-        };
-      };
-
+   */
     };
   };
 
+  /*
   security.acme.certs."hydra.satanic.link" =
     { email = "georgerw@gmail.com";
       postRun = ''systemctl reload nginx.service'';
     };
-
-  security.acme.certs."bstream.satanic.link" =
-    { email = "georgerw@gmail.com";
-      postRun = ''systemctl reload nginx.service'';
-    };
-
+*/
   services.prometheus.exporters = {
     nginx = {
       enable = true;
