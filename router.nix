ad6eb34ac64c5910eb4f19f7a0baab4a1cf139df
georgewhewell <georgerw@gmail.com>
2019-07-07 09:26:53 +0100

    redeploy router

--- router.nix
+++ router.nix
@@ -12,9 +12,11 @@
   services.haveged.enable = true;
   services.vnstat.enable = true;
   services.thermald.enable = true;
+  services.fstrim.enable = true;
 
   nix.maxJobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = "ondemand";
+  deployment.hasFastConnection = true;
 
   environment.systemPackages = with pkgs; [
     mosh
@@ -27,18 +29,20 @@
 
   fileSystems."/" =
     { device = "/dev/sda2";
-      fsType = "f2fs";
+      fsType = "ext4";
     };
 
-  services.consul.extraConfig = { server = true; bootstrap = true; };
-  services.consul.interface =
-    let interface = "br.lan"; in {
-      advertise = interface;
-      bind = interface;
-    };
+  services.consul = {
+    interface =
+      let interface = "br.lan"; in {
+        advertise = interface;
+        bind = interface;
+      };
+    extraConfig = { server = true; bootstrap_expect = 1; };
+  };
 
   networking = {
-    hostName = "router.lan";
+    hostName = "router";
     hostId = "deadbeef";
     enableIPv6 = false;
 
@@ -98,40 +102,41 @@
     };
   };
 
-    services.miniupnpd = {
-      enable = true;
-      externalInterface = "enp1s0";
-      internalIPs = [ "br.lan" ];
-      natpmp = true;
-      upnp = true;
-    };
+  services.miniupnpd = {
+    enable = true;
+    externalInterface = "enp1s0";
+    internalIPs = [ "br.lan" ];
+    natpmp = true;
+    upnp = true;
+  };
 
+  boot.kernelModules = [ "tcp_bbr" ];
+  boot.kernel.sysctl = {
+   "net.ipv4.tcp_congestion_control" = "bbr";
+   "net.core.default_qdisc" = "fq_codel";
+  };
 
-   boot.kernelModules = [ "tcp_bbr" ];
-   boot.kernel.sysctl = {
-     "net.ipv4.tcp_congestion_control" = "bbr";
-     "net.core.default_qdisc" = "fq_codel";
-   };
-
-   services.avahi.interfaces = [ "br.lan" ];
-   services.dnsmasq = {
+  services.avahi.interfaces = [ "br.lan" ];
+  services.dnsmasq = {
     enable = true;
     resolveLocalQueries = true;
-    servers = [ "8.8.8.8" "8.8.4.4" ];
+    servers = [ "1.1.1.1" ];
     extraConfig = ''
       domain-needed
       bogus-priv
       no-resolv
+      no-hosts
       log-dhcp
       domain=lan
       interface=lo
       interface=br.lan
-      dhcp-range=192.168.23.10,192.168.23.254,1h
-      dhcp-host=e4:8d:8c:a8:de:40,192.168.23.2  # switch
-      dhcp-host=80:2a:a8:80:96:ef,192.168.23.3  # ap
-      dhcp-host=0c:c4:7a:89:fb:37,192.168.23.4  # ipmi
-      dhcp-host=0c:c4:7a:87:b9:d8,192.168.23.5  # nixhost
+      dhcp-range=192.168.23.10,192.168.23.254,6h
+      dhcp-host=e4:8d:8c:a8:de:40,192.168.23.2   # switch
+      dhcp-host=80:2a:a8:80:96:ef,192.168.23.3   # ap
+      dhcp-host=0c:c4:7a:89:fb:37,192.168.23.4   # ipmi
+      dhcp-host=ce:f2:b6:d7:03:98,192.168.23.5   # nixhost
       dhcp-host=de:18:46:58:73:da,192.168.23.200 # plex
+      dhcp-host=f0:99:b6:42:49:05,192.168.23.48  # phone
 
       # hosted names
       address=/router.lan/192.168.23.1
@@ -139,11 +144,8 @@
       cname=hydra.satanic.link,nixhost.lan
       cname=grafana.satanic.link,nixhost.lan
       cname=git.satanic.link,nixhost.lan
-      cname=elk.satanic.link,nixhost.lan
-      cname=es.satanic.link,nixhost.lan
       cname=cache.satanic.link,nixhost.lan
     '';
-
   };
 
   nix.gc = {
