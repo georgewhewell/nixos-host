296d7570a6311ce9b66e4af6ada71773f4d505ca
georgewhewell <georgerw@gmail.com>
2019-07-08 11:45:51 +0100

    fix cache cache

--- services/nginx.nix
+++ services/nginx.nix
@@ -9,34 +9,11 @@
     recommendedTlsSettings = true;
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    virtualHosts = {
-/*      "hydra.satanic.link" = {
-         forceSSL = true;
-         enableACME = true;
-         locations."/" = {
-           extraConfig = ''
-            # Defer resolving upstream to allow start when upstream down
-            resolver 192.168.23.1 valid=30s;
-            set $upstream hydra.4a;
-            proxy_pass http://$upstream:3000;
-
-            proxy_set_header Host $host;
-            proxy_set_header X-Forwarded-For $remote_addr;
-            proxy_set_header X-Forwarded-Proto https;
-           '';
-         };
-      };
-   */
-    };
+    
+    # breaks cache-cache
+    # recommendedProxySettings = true;
   };
 
-  /*
-  security.acme.certs."hydra.satanic.link" =
-    { email = "georgerw@gmail.com";
-      postRun = ''systemctl reload nginx.service'';
-    };
-*/
   services.prometheus.exporters = {
     nginx = {
       enable = true;
