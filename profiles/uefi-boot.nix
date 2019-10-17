17e8e31d057678b2ab32c7079b17b2b26ff023e2
georgewhewell <georgerw@gmail.com>
2019-10-17 16:55:32 +0100

    uefi stuff

--- profiles/uefi-boot.nix
+++ profiles/uefi-boot.nix
@@ -1,46 +1,22 @@
 { config, pkgs, ... }:
 
 {
+
   hardware.enableAllFirmware = true;
   hardware.cpu.intel.updateMicrocode = true;
   security.rngd.enable = pkgs.lib.mkDefault true;
+  services.fwupd.enable = true;
 
   boot = {
     tmpOnTmpfs = true;
     kernelPackages = pkgs.linuxPackages_latest;
 
     loader = {
-      grub = {
-        efiSupport = true;
-        devices = [ "nodev" ];
-        zfsSupport = true;
-        copyKernels = true;
-	  extraEntries = ''
-	    menuentry "Reboot" {
-	      reboot
-	    }
-	    menuentry "Poweroff" {
-	      halt
-	    }
-	  '';
-      };
+      systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
 
     initrd = {
-   network = {
-     enable = true;
-     # This will use udhcp to get an ip address.
-     # Make sure you have added the kernel module for your network driver to `boot.initrd.availableKernelModules`, 
-     # so your initrd can load it!
-     # Static ip addresses might be configured using the ip argument in kernel command line:
-     # https://www.kernel.org/doc/Documentation/filesystems/nfs/nfsroot.txt
-     # this will automatically load the zfs password prompt on login
-     # and kill the other prompt so boot can continue
-     postCommands = ''
-       echo "zfs load-key -a; killall zfs" >> /root/.profile
-     '';
-   };
       supportedFilesystems = [
         "zfs"
         "nfs"
@@ -62,7 +38,5 @@
       ];
     };
   };
-
-  services.fwupd.enable = true;
 
 }
