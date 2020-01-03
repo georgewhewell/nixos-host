aea10c36fd04b516b868eb76b376b43863cb42fa
georgewhewell <georgerw@gmail.com>
2020-01-03 22:22:07 +0000

    waybar: show bandwidth

--- home/waybar.nix
+++ home/waybar.nix
@@ -61,9 +61,10 @@
     };
     memory = { format = "{}% "; };
     network = {
+      interval = 1;
       format-alt = "{ifname}: {ipaddr}/{cidr}";
       format-disconnected = "Disconnected ⚠";
-      format-ethernet = "{ifname}: {ipaddr}/{cidr} ";
+      format-ethernet = "{ifname}: {ipaddr}/{cidr}   up: {bandwidthUpBits} down: {bandwidthDownBits}";
       format-linked = "{ifname} (No IP) ";
       format-wifi = "{essid} ({signalStrength}%) ";
     };
