4a7032a3c9555214dacad7c4d31df73d67cd08da
georgewhewell <georgerw@gmail.com>
2020-01-05 18:22:48 +0000

    vim-test

--- home/vim.nix
+++ home/vim.nix
@@ -13,6 +13,7 @@
       gitgutter
       ale
 
+      vim-test
       denite
       denite-extra
     ];
@@ -23,6 +24,12 @@
       set noshowmode
       set number
 
+      nmap <silent> t<C-n> :TestNearest<CR>
+      nmap <silent> t<C-f> :TestFile<CR>
+      nmap <silent> t<C-s> :TestSuite<CR>
+      nmap <silent> t<C-l> :TestLast<CR>
+      nmap <silent> t<C-g> :TestVisit<CR>
+      
       let g:rustfmt_autosave = 1
       let g:racer_cmd = "/run/current-system/sw/bin/racer"
     '';
