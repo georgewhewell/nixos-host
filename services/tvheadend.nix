ffc2d509df1423b387a8f073d816978d81fb201f
georgewhewell <georgerw@gmail.com>
2020-01-09 23:26:29 +0000

    fix arm tbs build

--- services/tvheadend.nix
+++ services/tvheadend.nix
@@ -2,12 +2,23 @@
 
 {
 
-  /*
+  boot.kernelPatches = [
+    {
+      # Disable regular kernel em28xx module since dependencies will
+      # collide with other v4l2 from tbs modules
+      name = "disable em28xx";
+      patch = null;
+      extraConfig = ''
+        VIDEO_EM28XX n
+      '';
+    }
+  ];
+
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_4_19;
   boot.extraModulePackages = [
-    config.boot.kernelPackages.tbs
-    ];
-  */
+    pkgs.linuxPackages_4_19.tbs
+  ];
+
   environment.systemPackages = with pkgs; [ dtv-scan-tables ];
   hardware.firmware = [ pkgs.libreelec-dvb-firmware ];
   services.tvheadend.enable = true; 
