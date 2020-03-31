81b805c8c41ba4dd3772a0c60d098304a6d54d98
georgewhewell <georgerw@gmail.com>
2020-03-31 01:33:17 +0100

    need password store

--- home/common.nix
+++ home/common.nix
@@ -38,4 +38,10 @@
     enable = true;
   };
 
+  programs.password-store = {
+    enable = true;
+    settings = {
+      PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+    };
+  };
 }
