71a2a672961ac376ad6bad303f3df589a2070543
georgewhewell <georgerw@gmail.com>
2022-11-24 16:02:52 +0100

    cleanup

--- home/vscode-server.nix
+++ home/vscode-server.nix
@@ -2,13 +2,12 @@
 
   imports = [
     ./development.nix
-    # vscode-server.homeManagerModule
-    # "${fetchTarball
-    # "https://github.com/msteen/nixos-vscode-server/tarball/d2343b5eb47b811856085f3eff4d899a32b2c136"}/modules/vscode-server/home.nix"
+    (import "${pkgs.vscode-server-src}/modules/a").nixosModules.home
   ];
 
-  # services.vscode-server = {
-  #   enable = true;
-  #   # useFhsNodeEnvironment = false;
-  # };
+  services.vscode-server = {
+    enable = true;
+    # useFhsNodeEnvironment = false;
+  };
 }
+
