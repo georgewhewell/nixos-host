9f6a415c098cc0cf8ea8bb5c37964a5fda91ee57
georgewhewell <georgerw@gmail.com>
2023-01-22 16:53:04 +0100

    refactor lights

--- services/home-assistant/lights.nix
+++ services/home-assistant/lights.nix
@@ -3,24 +3,6 @@
 {
 
   services.home-assistant.config = {
-    # switch = [{
-    #   platform = "flux";
-    #   lights = [
-    #     "light.bedroom_ceiling_light"
-    #     "light.hallway_ceiling_light"
-    #     "light.office_ceiling_light"
-    #   ];
-    #   start_time = "7:00";
-    #   stop_time = "23:00";
-    #   start_colortemp = 8000;
-    #   sunset_colortemp = 2500;
-    #   stop_colortemp = 1000;
-    #   brightness = 100;
-    #   disable_brightness_adjust = false;
-    #   mode = "xy";
-    #   transition = 30;
-    #   interval = 60;
-    # }];
     adaptive_lighting = {
       lights = [
         "light.bedroom_ceiling_light"
@@ -29,95 +11,107 @@
       ];
     };
     automation =
+      let
+        mkMotionLight = { name, motionSensor, lightTarget, condition ? null }:
+          {
+            alias = "${name} Lights";
+            use_blueprint = {
+              path = "homeassistant/motion_light.yaml";
+              input = {
+                motion_entity = motionSensor;
+                light_target.area_id = lightTarget;
+              };
+            };
+            condition = condition;
+          };
+        mkRemoteToggle = { name, remote, lightTarget, action }:
+          {
+            alias = "${name} ${action}";
+            mode = "single";
+            trigger = {
+              device_id = remote;
+              domain = "zha";
+              type = "remote_button_short_press";
+              platform = "device";
+              subtype = "turn_${action}";
+            };
+            action = {
+              type = "turn_${action}";
+              device_id = lightTarget;
+              entity_id = "light.${name}";
+              domain = "light";
+            };
+          };
+      in
       [
-        /* Turn on lights with motion sensors */
-        {
-          alias = "Hallway Lights";
-          use_blueprint = {
-            path = "homeassistant/motion_light.yaml";
-            input = {
-              motion_entity = "binary_sensor.front_door_motion_sense_motion";
-              light_target.area_id = "hallway";
-            };
-          };
-        }
-        {
-          alias = "Office Lights";
-          use_blueprint = {
-            path = "homeassistant/motion_light.yaml";
-            input = {
-              motion_entity = "binary_sensor.office_motion_sensor_motion";
-              light_target.area_id = "office";
-            };
-          };
-        }
-        {
-          alias = "Bedroom Lights";
-          use_blueprint = {
-            path = "homeassistant/motion_light.yaml";
-            input = {
-              motion_entity = "binary_sensor.bedroom_motion_sensor_motion";
-              light_target.area_id = "605ca1bb58e342d6b7aeb66364977d62";
-            };
-          };
-          condition = [
-            # Dont activate during nighttime
-            {
-              alias = "Not in bed";
-              condition = "state";
-              entity_id = "binary_sensor.withings_in_bed_george";
-              state = "off";
-            }
-          ];
-        }
+        # Turn on lights with motion sensors
+        (mkMotionLight
+          {
+            name = "Hallway";
+            motionSensor = "binary_sensor.front_door_motion_sense_motion";
+            lightTarget = "hallway";
+          })
 
-        /* Control bedroom light with small remote */
+        (mkMotionLight
+          {
+            name = "Office";
+            motionSensor = "binary_sensor.office_motion_sensor_motion";
+            lightTarget = "office";
+          })
+
+        (mkMotionLight
+          {
+            name = "Bedroom";
+            motionSensor = "binary_sensor.bedroom_motion_sensor_motion";
+            lightTarget = "605ca1bb58e342d6b7aeb66364977d62";
+            condition = [
+              # Dont activate during nighttime
+              {
+                alias = "Not in bed";
+                condition = "state";
+                entity_id = "binary_sensor.withings_in_bed_george";
+                state = "off";
+              }
+            ];
+          })
+
+        # Turn on and off with small remote
+        (mkRemoteToggle
+          {
+            name = "bedroom_filament_light";
+            remote = "ee6328afcb13fd25142e3745ea7697b5";
+            lightTarget = "a6a9740d1a1b5212b8ba8ccd41840eed";
+            action = "on";
+          })
+
+        (mkRemoteToggle
+          {
+            name = "bedroom_filament_light";
+            remote = "ee6328afcb13fd25142e3745ea7697b5";
+            lightTarget = "a6a9740d1a1b5212b8ba8ccd41840eed";
+            action = "off";
+          })
+
+        # Turn off all lights when small remote long pressed
         {
-          alias = "Small Remote Off";
-          mode = "single";
+          alias = "Turn off all lights";
           trigger = {
             device_id = "ee6328afcb13fd25142e3745ea7697b5";
             domain = "zha";
-            type = "remote_button_short_press";
+            type = "remote_button_long_press";
             platform = "device";
-            subtype = "turn_off";
           };
           action = {
-            type = "turn_off";
-            device_id = "a6a9740d1a1b5212b8ba8ccd41840eed";
-            entity_id = "light.bedroom_filament_light";
-            domain = "light";
-          };
-        }
-        {
-          alias = "Small Remote On";
-          mode = "single";
-          trigger = {
-            device_id = "ee6328afcb13fd25142e3745ea7697b5";
-            domain = "zha";
-            platform = "device";
-            type = "remote_button_short_press";
-            subtype = "turn_on";
-          };
-          action = {
-            type = "turn_on";
-            device_id = "a6a9740d1a1b5212b8ba8ccd41840eed";
-            entity_id = "light.bedroom_filament_light";
-            domain = "light";
+            service = "scene.turn_on";
+            target = {
+              entity_id = "scene.zzz";
+            };
+            data = {
+              transition = "2.5";
+            };
           };
         }
       ];
-    scene = [
-      {
-        name = "Zzz";
-        entities =
-          {
-            "light.bedroom_ceiling_light" = "off";
-            "light.hallway_ceiling_light" = "off";
-            "light.office_ceiling_light" = "off";
-            "light.bedroom_filament_light" = "off";
-          };
-      }
-    ];
+
   };
 }
