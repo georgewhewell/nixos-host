1a1ce87768341cd1a4610273bbd06cc6944f0ea0
georgewhewell <georgerw@gmail.com>
2024-04-07 13:24:43 +0200

    vscode server

--- home/vscode-server.nix
+++ home/vscode-server.nix
@@ -2,18 +2,12 @@
 
   imports = [
     ./development.nix
-    # pkgs.vscode-server-src
-    # inputs.nixos-vscode-server.homeModules
-    # "${fetchTarball {
-    #   url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
-    #   sha256 = "sha256:0sz8njfxn5bw89n6xhlzsbxkafb6qmnszj4qxy2w0hw2mgmjp829";
-    # }}/modules/vscode-server/home.nix"
+    inputs.vscode-server.homeModules.default
   ];
 
-  # services.vscode-server = {
-  #   enable = true;
-  #   #useFhsNodeEnvironment = false;
-  # };
+  services.vscode-server = {
+    enable = true;
+  };
 }
 
 
